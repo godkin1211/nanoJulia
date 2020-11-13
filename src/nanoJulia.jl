045615366af0073892547793ad2cea0047ac0292
米夏糯 <godkin@mixianuodeMac-mini.local>
2020-11-13 16:06:17 +0800

    nanoJulia.jl:Add some context. Project.toml: Update info.

--- src/nanoJulia.jl
+++ src/nanoJulia.jl
@@ -1,5 +1,19 @@
 module nanoJulia
+using Statistics
+export calculate_mean_readqual
 
-# Write your package code here.
+phred2prob(q::UInt8) = 10^(q/(-10))
+
+prob2phred(p::Float64) = -10log10(p)
+
+function calculate_mean_readqual(quals::Array{UInt8, 1})::Float64
+	quals .|> phred2prob |> mean |> prob2phred |> q->round(q, digits = 1)
+end
+
+function remove_bad_read!(records::Array{FASTX.FASTQ.Record, 1})
+	seqlengths = FASTQ.seqlen.(records)
+	quallengths = map(i -> length(FASTQ.quality(i)), records)
+	return records[seqlengths .==  quallengths]
+end
 
 end
