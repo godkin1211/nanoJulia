984dffdb03834e0a15e04344983c7b96eba72b5c
Chiachun Chiu <jiajyun.ciou@gmail.com>
2020-12-05 21:04:12 +0800

    statistics_handler.jl & info_handler.jl: modifiy come codes.

--- src/info_handler.jl
+++ src/info_handler.jl
@@ -2,15 +2,15 @@ function extract_info end
 
 # Extract info from FastqInfo
 function extract_info(readsinfo::Array{Union{FastqInfo,Missing},1})
-	quals = map(e -> e.quality, readsinfo);
-	lengths = map(e -> e.length, readsinfo);
+	quals = map(e -> round(e.quality, digits=1), readsinfo)
+	lengths = map(e -> e.length, readsinfo)
 	DataFrame(quality = quals, length = lengths)
 end
 
 # Extract info from BAMInfo
 function extract_info(readsinfo::Array{BAMInfo,1})
-	quals = map(e -> e.quality, readsinfo);
-	lengths = map(e -> e.length, readsinfo);
-	identities = map(e -> round(e.identity, digits=1), readsinfo);
+	quals = map(e -> round(e.quality, digits=1), readsinfo)
+	lengths = map(e -> e.length, readsinfo)
+	identities = map(e -> round(e.identity, digits=1), readsinfo)
 	DataFrame(quality = quals, length = lengths, identity = identities)
 end
--- src/statistics_handler.jl
+++ src/statistics_handler.jl
@@ -1,14 +1,22 @@
 # Subsetting info-table with quality
-getQScorePart(df::DataFrame, qscore::Int64) = df[df.quality .>= qscore, :]
+function getQScoreOverQ(df::DataFrame, qscore::Int64, totallength::Int64) 
+	df_subset = df[df.quality .>= qscore, :]
+	readNum = size(df_subset, 1)
+	baseNum = totalLen(df_subset.length)
+	basePerc = round(baseNum * 100 / totallength, digits=2)
+	return(readNum, baseNum, "$basePerc%")
+end
 
+# Plotting functions
 function plotSquiggle(signals::Array{Int16,1})
 	gr()
 	timepoints = [i for i in 1:length(signals)] ./ 4000
 	plot(timepoints, signals, w=3, xlabel = "Time (seconds)", ylabel= "Detected Current (pA)", legend = false)
 end
 
-function plotReadLen2QualScatter(df::DataFrames.DataFrame, N50::Int64)
+function plotReadLen2QualScatter(df::DataFrames.DataFrame, N50::Int64, outputDir::String)
 	gr()
+	outputfile = joinpath(outputDir, "read_length_vs_quality_dot.png")
 	if "identity" in names(df)
 		plot(df.length,
 			df.quality,
@@ -30,18 +38,25 @@ function plotReadLen2QualScatter(df::DataFrames.DataFrame, N50::Int64)
 			dpi = 300)
 	end
 	vline!([N50], lw = 2, lab = "N50")
-	savefig("read_length_vs_quality_dot.png")
+	savefig(outputfile)
 end
 
-function plotReadLen2QualHistogram2D(df::DataFrames.DataFrame)
+function plotReadLen2QualHistogram2D(df::DataFrames.DataFrame, outputDir::String)
 	gr()
+	outputfile = joinpath(outputDir, "read_length_vs_quality_histogram.png")
 	historgam2d(df.length, df.quality, nbins = 50)
-	savefig("read_length_vs_quality_histogram.png")
+	savefig(outputfile)
 end
 
+
+# Generating statics summary files
 function generateStatSummary(df::DataFrames.DataFrame, totalLength::Int64, N50::Int64, outputDir::String)::DataFrames.DataFrame
 	stat_summary = describe(df)
 	readNum = size(df, 1)
+	readsQ7, basesQ7, percQ7 = getQScoreOverQ(df, 7, totalLength)
+	readsQ10, basesQ10, percQ10 = getQScoreOverQ(df, 10, totalLength)
+	readsQ12, basesQ12, percQ12 = getQScoreOverQ(df, 12, totalLength)
+	readsQ15, basesQ15, percQ15 = getQScoreOverQ(df, 15, totalLength)
 	output_df = DataFrame(Property = AbstractString[], Value = Number[])
 	if ncol(df) == 3
 		meanQual, meanLen, meanIdent = tuple(round.(stat_summary[!,:mean], digits=1)...)
@@ -61,22 +76,41 @@ function generateStatSummary(df::DataFrames.DataFrame, totalLength::Int64, N50::
 	push!(output_df, ("Read N50", N50))
 	push!(output_df, ("Read Numbers", readNum))
 	push!(output_df, ("Total Bases", totalLength))
-	meanQualtxt = @sprintf "Mean Read Quality: %20.1f" meanQual
-	meanLentxt = @sprintf "Mean Read Length: %21.1f" meanLen
-	medianQualtxt = @sprintf "Median Read Quality: %18.1f" medianQual
-	medianLentxt = @sprintf "Median Read Length: %19.1f" medianLen
-	readN50txt = @sprintf "Read N50: %29s" format(N50, commas=true)
-	readNumtxt = @sprintf "Read Number: %26s" format(readNum, commas=true)
-	totalBasestxt = @sprintf "Total Bases: %26s" format(totalLength, commas=true)
+
+	meanQualtxt = @sprintf "Mean Read Quality: %27.1f" meanQual
+	meanLentxt = @sprintf "Mean Read Length: %28.1f" meanLen
+	medianQualtxt = @sprintf "Median Read Quality: %25.1f" medianQual
+	medianLentxt = @sprintf "Median Read Length: %26.1f" medianLen
+	readN50txt = @sprintf "Read N50: %36s" format(N50, commas=true)
+	readNumtxt = @sprintf "Read Number: %33s" format(readNum, commas=true)
+	totalBasestxt = @sprintf "Total Bases: %33s" format(totalLength, commas=true)
+	readsQ7txt = @sprintf "Reads with quality score > 7: %16s" format(readsQ7, commas=true)
+	basesQ7txt = @sprintf "Bases with quality score > 7: %16s" format(basesQ7, commas=true)
+	percQ7txt = @sprintf "Percentage of bases (Q>7): %20s" percQ7
+	readsQ10txt = @sprintf "Reads with quality score > 10: %15s" format(readsQ10, commas=true)
+	basesQ10txt = @sprintf "Bases with quality score > 10: %15s" format(basesQ10, commas=true)
+	percQ10txt = @sprintf "Percentage of bases (Q>10): %20s" percQ10
+	readsQ12txt = @sprintf "Reads with quality score > 12: %15s" format(readsQ12, commas=true)
+	basesQ12txt = @sprintf "Bases with quality score > 12: %15s" format(basesQ12, commas=true)
+	percQ12txt = @sprintf "Percentage of bases (Q>12): %20s" percQ12
+	readsQ15txt = @sprintf "Reads with quality score > 15: %15s" format(readsQ15, commas=true)
+	basesQ15txt = @sprintf "Bases with quality score > 15: %15s" format(basesQ15, commas=true)
+	percQ15txt = @sprintf "Percentage of bases (Q>15): %20s" percQ15
+
 	outputfile = joinpath(outputDir, "statistics_summary.txt")
-	if ncol(df) == 3
-		open(outputfile, "w") do io
+	open(outputfile, "w") do io
+		if ncol(df) == 3
 			write(io, "$meanQualtxt\n$meanLentxt\n$meanIdenttxt\n$medianQualtxt\n$medianLentxt\n$medianIdenttxt\n$readN50txt\n$readNumtxt\n$totalBasestxt")
-		end
-	else
-		open(outputfile, "w") do io
+		else
 			write(io, "$meanQualtxt\n$meanLentxt\n$medianQualtxt\n$medianLentxt\n$readN50txt\n$readNumtxt\n$totalBasestxt")
 		end
+		write(io, "$readsQ7txt\n$basesQ7txt\n$percQ7txt")
+		write(io, "$readsQ10txt\n$basesQ10txt\n$percQ10txt")
+		write(io, "$readsQ12txt\n$basesQ12txt\n$percQ12txt")
+		write(io, "$readsQ15txt\n$basesQ15txt\n$percQ15txt")
 	end
+	
+	plotReadLen2QualScatter(df, N50, outputDir)
+	plotReadLen2QualHistogram2D(df, outputDir)
 	return output_df
 end
